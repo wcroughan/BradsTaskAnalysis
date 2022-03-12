b3cc167198c10f46a97203812885e907dce4fb5c
wcroughan <croughanb@gmail.com>
2022-03-12 13:21:31 -0800

    update

--- TrodesCameraExtrator.py
+++ TrodesCameraExtrator.py
@@ -15,15 +15,25 @@ from consts import TRODES_SAMPLING_RATE
 from BTData import BTData
 
 
-def getFrameBatch(videoFileName, startFrame, numFrames=100):
-    process = (
-        ffmpeg
-        .input(videoFileName)
-        .video
-        .filter('select', 'gte(n,{})'.format(startFrame))
-        .output('pipe:', format='rawvideo', vframes=numFrames, pix_fmt='rgb24')
-        .run_async(pipe_stdin=True, pipe_stdout=True)
-    )
+def getFrameBatch(videoFileName, startFrame, numFrames=None, frameStride=1):
+    if numFrames is None:
+        process = (
+            ffmpeg
+            .input(videoFileName)
+            .video
+            .filter('select', 'gte(n,{})*not(mod(n,{}))'.format(startFrame, frameStride))
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24', vsync="vfr")
+            .run_async(pipe_stdin=True, pipe_stdout=True)
+        )
+    else:
+        process = (
+            ffmpeg
+            .input(videoFileName)
+            .video
+            .filter('select', 'gte(n,{})*not(mod(n,{}))'.format(startFrame, frameStride))
+            .output('pipe:', format='rawvideo', vframes=numFrames, pix_fmt='rgb24', vsync="vfr")
+            .run_async(pipe_stdin=True, pipe_stdout=True)
+        )
 
     stream = io.BytesIO()
     stream.seek(0)
@@ -40,6 +50,25 @@ def processRawTrodesVideo(videoFileName, timestampFileName=None, lightOffThresho
                           threshold=50, searchDist=100, showVideo=False, frameBatchSize=1000,
                           maxNumBatches=None, outputFileName=None, batchStart=0,
                           overwriteMode="ask", lightOnThreshold=0.2):
+    if outputFileName is None:
+        outputFileName = '.'.join(videoFileName.split('.')[0:-1]) + '.videoPositionTracking'
+
+    if os.path.exists(outputFileName):
+        if overwriteMode == "never":
+            return outputFileName
+
+        elif overwriteMode == "rename":
+            outputFileName += ".tce"
+
+        elif overwriteMode == "ask":
+            c = input("Output file {} exists, overwrite? (y/N/l):".format(outputFileName))
+            if c != "y":
+                return outputFileName
+
+        elif overwriteMode != "always":
+            print("Unknown overwrite mode {}".format(overwriteMode))
+            return outputFileName
+
     probe = ffmpeg.probe(videoFileName)
     video_stream = next(
         (stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
@@ -72,25 +101,6 @@ def processRawTrodesVideo(videoFileName, timestampFileName=None, lightOffThresho
             if iter > max_iter:
                 raise Exception
         timeStamps = np.fromfile(tsFile, np.uint32)
-
-    if outputFileName is None:
-        outputFileName = '.'.join(videoFileName.split('.')[0:-1]) + '.videoPositionTracking'
-
-    if os.path.exists(outputFileName):
-        if overwriteMode == "never":
-            return
-
-        elif overwriteMode == "rename":
-            outputFileName += ".tce"
-
-        elif overwriteMode == "ask":
-            c = input("Output file {} exists, overwrite? (y/N):".format(outputFileName))
-            if c != "y":
-                return
-
-        elif overwriteMode != "always":
-            print("Unknown overwrite mode {}".format(overwriteMode))
-            return
 
     outputDataType = np.dtype([('timestamp', np.uint32), ('x1', np.uint16),
                                ('y1', np.uint16), ('x2', np.uint16), ('y2', np.uint16)])
@@ -304,15 +314,118 @@ def runAllBradTask():
         processRawTrodesVideo(videoName, overwriteMode="never")
 
 
-def getFrameColorHeuristic(frame, greyDiffThreshLow=25, greyDiffThreshHigh=100):
-    gdiff = frame[:, :, 0] - frame[:, :, 1]
-    return np.count_nonzero(np.logical_and(
-        gdiff > greyDiffThreshLow, gdiff < greyDiffThreshHigh))
+def getFrameColorHeuristic(frame, greyDiffThreshLow=25, greyDiffThreshHigh=200):
+    diff1 = frame[:, :, 0] - frame[:, :, 1]
+    diff2 = frame[:, :, 0] - frame[:, :, 2]
+    diff3 = frame[:, :, 1] - frame[:, :, 2]
+    h1 = np.histogram(diff1, bins=[0, greyDiffThreshLow, greyDiffThreshHigh, 1000])
+    h2 = np.histogram(diff2, bins=[0, greyDiffThreshLow, greyDiffThreshHigh, 1000])
+    h3 = np.histogram(diff3, bins=[0, greyDiffThreshLow, greyDiffThreshHigh, 1000])
+    # return np.count_nonzero(np.logical_and(
+    # gdiff > greyDiffThreshLow, gdiff < greyDiffThreshHigh))
+    return h1[0], h2[0], h3[0]
+
+
+def findLightTimesLinear(videoFileName, frameStart, frameEnd, frameStride, findLightOff=True, findLightOn=True,
+                         showVideo=False, lightOnThreshold=0.2, lightOffThreshold=0.1):
+    """
+    making a separate function here b/c hopefully that means freed memory?
+    """
+    probe = ffmpeg.probe(videoFileName)
+    video_stream = next(
+        (stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    width = int(video_stream['width'])
+    height = int(video_stream['height'])
+
+    lightOffThreshold *= 255 * width * height
+    lightOnThreshold *= 255 * width * height
+
+    if frameEnd is None:
+        batch = getFrameBatch(videoFileName, frameStart, frameStride=frameStride)
+    else:
+        nFrames = (frameEnd - frameStart) // frameStride
+        batch = getFrameBatch(videoFileName, frameStart, numFrames=nFrames, frameStride=frameStride)
+    frames = np.frombuffer(batch, np.uint8).reshape((-1, height, width, 3))
+    nFrames = frames.shape[0]
+
+    allFrameH = np.array([getFrameColorHeuristic(
+        frames[i, :, :, :].reshape(height, width, 3))[1][1] for i in range(nFrames)])
+    print(allFrameH)
+    maxH = np.max(allFrameH)
+    minH = np.min(allFrameH)
+    thresh = (maxH + minH) / 2.0
+
+    if showVideo and False:
+        for frameI in range(nFrames):
+            if frameI < 15 or nFrames - frameI < 15:
+                frame = frames[frameI, :, :, :].reshape((height, width, 3))
+                heur = getFrameColorHeuristic(frame)
+                print(heur)
+
+        for frameI in range(nFrames):
+            if frameI < 15 or nFrames - frameI < 15:
+                frame = frames[frameI, :, :, :].reshape((height, width, 3))
+                heur = getFrameColorHeuristic(frame)
+                frame = cv2.putText(frame, "{}: h={}".format(frameI, heur), (100, 100),
+                                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+                # frame = cv2.putText(frame, "thresh={}, loffF2={}, lonF2={}".format(thresh, loffF2, lonF2), (100, 200),
+                # cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+                cv2.imshow("frame", frame)
+                if cv2.waitKey() & 0xFF == ord('q'):
+                    break
+        cv2.destroyAllWindows()
+
+    loffSet = False
+    lonSet = False
+    loffF1 = None
+    loffF2 = None
+    lonF1 = None
+    lonF2 = None
+
+    for i in range(1, nFrames):
+        if allFrameH[i] < thresh and allFrameH[i-1] > thresh and not loffSet and findLightOff:
+            loffF1 = i - 1
+            loffF2 = i
+            loffSet = True
+            if not findLightOn:
+                break
+        if allFrameH[i] > thresh and allFrameH[i-1] < thresh and findLightOn:
+            lonF1 = i - 1
+            lonF2 = i
+            lonSet = True
+            if not findLightOff:
+                break
+
+    if not lonSet and findLightOn:
+        lonF1 = nFrames - 1
+        lonF2 = nFrames
+
+    if showVideo and True:
+        for frameI in range(nFrames):
+            if frameI < 15 or nFrames - frameI < 15:
+                frame = frames[frameI, :, :, :].reshape((height, width, 3))
+                heur = getFrameColorHeuristic(frame)
+                print(heur)
+
+        for frameI in range(nFrames):
+            frame = frames[frameI, :, :, :].reshape((height, width, 3))
+            # heur = getFrameColorHeuristic(frame)
+            heur = allFrameH[frameI]
+            frame = cv2.putText(frame, "{}: h={}".format(frameI, heur), (100, 100),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+            frame = cv2.putText(frame, "thresh={}, loffF2={}, lonF2={}".format(thresh, loffF2, lonF2), (100, 200),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+            cv2.imshow("frame", frame)
+            if cv2.waitKey() & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+
+    return loffF1, loffF2, lonF1, lonF2
 
 
 def processUSBVideoData(videoFileName, batchStart=0, frameBatchSize=1000,
                         greyDiffThreshHigh=100, greyDiffThreshLow=25, lightOnThreshold=0.1, lightOffThreshold=0.04,
-                        maxNumBatches=None, showVideo=False, outputFileName=None, overwriteMode="ask", initialSkipAmt=12):
+                        maxNumBatches=None, showVideo=False, outputFileName=None, overwriteMode="ask", initialSkipAmt=8):
     probe = ffmpeg.probe(videoFileName)
     video_stream = next(
         (stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
@@ -359,61 +472,30 @@ def processUSBVideoData(videoFileName, batchStart=0, frameBatchSize=1000,
             return
 
     initialFrameJump = np.power(2, initialSkipAmt)
-    loffF1 = 0
-    loffF2 = initialFrameJump
-    while True:
-        batch = getSingleFrame(videoFileName, loffF2)
-        batchBufferIdxJump = width * height * 3
-        frame = batch[0:batchBufferIdxJump]
-        npframe = np.frombuffer(frame, np.uint8).reshape((height, width, 3))
-        numPxWithColor = getFrameColorHeuristic(npframe)
-        if numPxWithColor < lightOffThreshold:
-            break
-        loffF2 += initialFrameJump
 
-    while loffF2 - loffF1 > 1:
-        midIdx = loffF1 + int((loffF2 - loffF1) / 2)
-        print(loffF1, midIdx, loffF2)
-        batch = getSingleFrame(videoFileName, loffF2)
-        batchBufferIdxJump = width * height * 3
-        frame = batch[0:batchBufferIdxJump]
-        npframe = np.frombuffer(frame, np.uint8).reshape((height, width, 3))
-        numPxWithColor = getFrameColorHeuristic(npframe)
-        if numPxWithColor < lightOffThreshold:
-            # in left half
-            loffF2 = midIdx
-        else:
-            loffF1 = midIdx
-    lightsOffFrame = loffF1
+    # Step one: find left and right bound on light off and on time
+    loffF1, loffF2, lonF1, lonF2 = findLightTimesLinear(
+        videoFileName, 0, None, initialFrameJump, showVideo=showVideo)
+    loffF1 *= initialFrameJump
+    loffF2 *= initialFrameJump
+    lonF1 *= initialFrameJump
+    lonF2 *= initialFrameJump
 
-    lonF1 = loffF2
-    lonF2 = lonF1 + initialFrameJump
-    while True:
-        batch = getSingleFrame(videoFileName, loffF2)
-        batchBufferIdxJump = width * height * 3
-        frame = batch[0:batchBufferIdxJump]
-        if not frame:
-            break
-        npframe = np.frombuffer(frame, np.uint8).reshape((height, width, 3))
-        numPxWithColor = getFrameColorHeuristic(npframe)
-        if numPxWithColor > lightOnThreshold:
-            break
-        loffF2 += initialFrameJump
+    print(loffF1, loffF2, lonF1, lonF2)
 
-    while lonF2 - lonF1 > 1:
-        midIdx = lonF1 + int((lonF2 - lonF1) / 2)
-        print(lonF1, midIdx, lonF2)
-        batch = getSingleFrame(videoFileName, loffF2)
-        batchBufferIdxJump = width * height * 3
-        frame = batch[0:batchBufferIdxJump]
-        npframe = np.frombuffer(frame, np.uint8).reshape((height, width, 3))
-        numPxWithColor = getFrameColorHeuristic(npframe)
-        if numPxWithColor < lightOnThreshold:
-            # in left half
-            lonF2 = midIdx
-        else:
-            lonF1 = midIdx
-    lightsOnFrame = lonF1
+    # Step two: for each of those intervals, get the video in that range and binary search that ish
+    # lightsOffFrame = findLightTimesBinary(
+    #     videoFileName, loffF1, loffF2, 1, findLightOff=True, showVideo=showVideo)
+    # lightsOnFrame = findLightTimesBinary(
+    #     videoFileName, lonF1, lonF2, 1, findLightOn=True, showVideo=showVideo)
+    _, lightsOffFrame, _, _ = findLightTimesLinear(
+        videoFileName, loffF1, loffF2, 1, showVideo=showVideo, findLightOn=False)
+    lightsOffFrame += loffF1
+    _, _, _, lightsOnFrame = findLightTimesLinear(
+        videoFileName, lonF1, lonF2, 1, showVideo=showVideo, findLightOff=False)
+    lightsOnFrame += lonF1
+
+    print(lightsOffFrame, lightsOnFrame)
 
     outArr = np.array([lightsOffFrame, lightsOnFrame])
     with open(outputFileName, "w") as outFile:
@@ -432,6 +514,7 @@ def playFrames(videoFileName, frameStart, frameEnd):
     batch = getFrameBatch(videoFileName, frameStart, numFrames=frameEnd - frameStart)
     batchBufferIdx = 0
     batchBufferIdxJump = width * height * 3
+    quitReq = False
     while True:
         frame = batch[batchBufferIdx:batchBufferIdx + batchBufferIdxJump]
         batchBufferIdx += batchBufferIdxJump
@@ -441,39 +524,60 @@ def playFrames(videoFileName, frameStart, frameEnd):
         npframe = np.frombuffer(frame, np.uint8).reshape((height, width, 3))
         cv2.imshow("frame", npframe)
         if cv2.waitKey(50) & 0xFF == ord('q'):
+            quitReq = True
             break
+
+    if not quitReq:
+        cv2.waitKey()
+
+    cv2.destroyAllWindows()
 
 
 def testUSBAlignment():
     usbVideoFile = "/home/wcroughan/Desktop/2021-12-09_18-04-47.mkv"
-    usbLightOffFrame, usbLightOnFrame = processUSBVideoData(usbVideoFile, overwriteMode="ask")
+    usbLightOffFrame, usbLightOnFrame = processUSBVideoData(
+        usbVideoFile, overwriteMode="loadOld", showVideo=False)
     print(usbLightOffFrame, usbLightOnFrame)
-    playFrames(usbVideoFile, usbLightOffFrame - 20, usbLightOffFrame + 20)
-    playFrames(usbVideoFile, usbLightOnFrame - 20, usbLightOnFrame + 20)
+    # playFrames(usbVideoFile, usbLightOffFrame - 20, usbLightOffFrame + 20)
+    # playFrames(usbVideoFile, usbLightOnFrame - 20, usbLightOnFrame + 20)
     sesh_name = "20211209_180352"
-    dataFileName = "/media/WDC6/B13/processed_data/B13_bradtask.dat"
-    quit()
+    dataFileName = "/media/WDC7/B13/processed_data/B13_bradtask.dat"
     ratData = BTData()
     ratData.loadFromFile(dataFileName)
     sesh = ratData.getSessions(lambda s: s.name == sesh_name)[0]
     trodesVideoFile = "/media/WDC6/B13/bradtasksessions/20211209_180352/20211209_180352.1.h264"
-    processedTrodesVideoFileName = processRawTrodesVideo(trodesVideoFile, overwriteMode="rename")
+    trodesOutputFileName = '.'.join(trodesVideoFile.split('.')[0:-1]) + '.videoPositionTracking.tce'
+    processedTrodesVideoFileName = processRawTrodesVideo(
+        trodesVideoFile, overwriteMode="never", outputFileName=trodesOutputFileName)
+    print("trodesVideoOutput in {}".format(processedTrodesVideoFileName))
     settings, position_data = readRawPositionData(processedTrodesVideoFileName)
 
     trodesLightOnFrame = None
     trodesLightOffFrame = None
     for s in settings:
-        if "lightOnFrame" in s:
-            trodesLightOnFrame = int(s.split(":")[1].strip())
-        if "lightOffFrame" in s:
-            trodesLightOffFrame = int(s.split(":")[1].strip())
+        s = str(s).lower()
+        print(s)
+        if "lightonframe" in s:
+            ss = s.split(":")[1].strip()
+            if ss[-3:] == "\\n'":
+                ss = ss[0:-3]
+            print(ss)
+            trodesLightOnFrame = int(ss)
+        if "lightoffframe" in s:
+            ss = s.split(":")[1].strip()
+            if ss[-3:] == "\\n'":
+                ss = ss[0:-3]
+            print(ss)
+            trodesLightOffFrame = int(ss)
+    print(trodesLightOnFrame, trodesLightOffFrame)
 
     trodesLightOnTime = position_data[trodesLightOnFrame]['timestamp']
     trodesLightOffTime = position_data[trodesLightOffFrame]['timestamp']
+    print(trodesLightOnTime, trodesLightOffTime)
 
     bufferTime = 1
 
-    for wft, wlt in zip(sesh.home_well_find_times, home_well_leave_times):
+    for wft, wlt in reversed(list(zip(sesh.home_well_find_times, sesh.home_well_leave_times))):
         t1 = wft - bufferTime * TRODES_SAMPLING_RATE
         t2 = wlt + bufferTime * TRODES_SAMPLING_RATE
         print(t1, t2)
@@ -481,7 +585,7 @@ def testUSBAlignment():
         wellFrames = np.floor(np.interp([t1, t2], [trodesLightOffTime, trodesLightOnTime],
                                         [usbLightOffFrame, usbLightOnFrame])).astype(int)
 
-        playFrames(videoFileName, wellFrames[0], wellFrames[1])
+        playFrames(usbVideoFile, wellFrames[0], wellFrames[1])
 
 
 if __name__ == "__main__":
