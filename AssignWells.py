5904360001d20b61702bdbb15c9a40cfcd0d35e0
wcroughan <croughanb@gmail.com>
2022-06-21 21:12:55 -0700

    jiojoijoij

--- AssignWells.py
+++ AssignWells.py
@@ -44,6 +44,7 @@ working_wells = set(all_wells) - set(broken_wells)
 
 probeFillTimes = np.linspace(60, 60*4, 4)
 
+
 def quadrant(well):
     if well < 25:
         if well % 8 in [2, 3, 4]:
@@ -55,6 +56,7 @@ def quadrant(well):
             return 3
         else:
             return 4
+
 
 ENFORCE_HOME_OFF_WALL = True
 WALL_WELLS = [str(i) for i in all_wells if i < 9 or i > 40 or i % 8 in [2, 7]]
@@ -68,8 +70,9 @@ OFFWALLQ2 = [w for w in q2 if w in OFF_WALL_WELLS]
 OFFWALLQ3 = [w for w in q3 if w in OFF_WALL_WELLS]
 OFFWALLQ4 = [w for w in q4 if w in OFF_WALL_WELLS]
 
+
 def wellGridString(homewell, wellnums):
-    wellgrid = np.array(['.'] * 36).reshape(6,6)
+    wellgrid = np.array(['.'] * 36).reshape(6, 6)
     for wn in wellnums:
         wellgrid[int(wn) // 8, (int(wn)-2) % 8] = "a"
     if homewell is not None:
@@ -86,6 +89,7 @@ if TASK_VERSION == 1:
     ))
     exit()
 
+
 def approveHomeWells(home_wells):
     # if quadrant(home_wells[0]) != 1 or quadrant(home_wells[1]) != 4:
     #     return False
@@ -96,17 +100,16 @@ def approveHomeWells(home_wells):
         i += 2
 
     return any([w in q1 for w in home_wells]) and \
-            any([w in q2 for w in home_wells]) and \
-            any([w in q3 for w in home_wells]) and \
-            any([w in q4 for w in home_wells])
+        any([w in q2 for w in home_wells]) and \
+        any([w in q3 for w in home_wells]) and \
+        any([w in q4 for w in home_wells])
 
-    
 
 if TASK_VERSION == 0:
     thisday = first_day
     ratOrder = [v for v in ratNames]
 
-    NUM_AWAY_WELLS_PER_SESSION = 9
+    NUM_AWAY_WELLS_PER_SESSION = 12
 
     if NUM_DAYS * RUNS_PER_DAY > len(OFF_WALL_WELLS):
         raise Exception("UNIMPLEMENTED: too many days")
@@ -114,10 +117,11 @@ if TASK_VERSION == 0:
     if ENFORCE_HOME_OFF_WALL:
         # home_wells = rd.sample(OFF_WALL_WELLS, NUM_DAYS * RUNS_PER_DAY)
         # while not approveHomeWells(home_wells):
-            # home_wells = rd.sample(OFF_WALL_WELLS, NUM_DAYS * RUNS_PER_DAY)
+        # home_wells = rd.sample(OFF_WALL_WELLS, NUM_DAYS * RUNS_PER_DAY)
 
         n = math.ceil(NUM_DAYS * RUNS_PER_DAY / 4)
-        home_wells = rd.sample(OFFWALLQ1, n) + rd.sample(OFFWALLQ2, n) + rd.sample(OFFWALLQ3, n) + rd.sample(OFFWALLQ4, n)  
+        home_wells = rd.sample(OFFWALLQ1, n) + rd.sample(OFFWALLQ2, n) + \
+            rd.sample(OFFWALLQ3, n) + rd.sample(OFFWALLQ4, n)
         while not approveHomeWells(home_wells):
             rd.shuffle(home_wells)
         home_wells = home_wells[0:NUM_DAYS * RUNS_PER_DAY]
@@ -135,14 +139,12 @@ if TASK_VERSION == 0:
             rd.shuffle(ctrlFillTimes)
             rd.shuffle(swrFillTimes)
 
-
         if di % 4 in [0, 2]:
             tf = rd.random() < 0.5
             this_day_condition_order = [tf]
             this_day_condition_order.append(tf if di == 2 else not tf)
         else:
             this_day_condition_order = [not tf for tf in this_day_condition_order]
-
 
         # if di % 4 == 0:
         #     rd.shuffle(day_condition_order)
@@ -195,7 +197,6 @@ if TASK_VERSION == 0:
             if PRINT_WELL_GRID:
                 print(wellGridString(wells[0], wells[1:]))
 
-
             # if PRINT_DAY_WELL_GRID:
             daywells = daywells | set(wells)
 
@@ -220,7 +221,6 @@ if TASK_VERSION == 0:
                         f.write("\n\n" + wellGridString(None, set(daywells)) + "\n")
                         for _ in range(3):
                             f.write("\n")
-
 
         if PRINT_DAY_WELL_GRID:
             print("DAY GRID: {}".format(thisday.strftime("%Y%m%d")))
