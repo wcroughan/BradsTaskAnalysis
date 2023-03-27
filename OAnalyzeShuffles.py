c2bd6fc1c486129cc05b0abd790395edc1495749
Bill Croughan <croughanb@gmail.com>
2023-03-26 22:34:04 -0700

    updates from linux desktop

--- OAnalyzeShuffles.py
+++ OAnalyzeShuffles.py
@@ -3,6 +3,7 @@ import os
 from itertools import product
 from pprint import pprint
 import random
+import pandas as pd
 
 from Shuffler import Shuffler
 from BTSession import BTSession
@@ -32,12 +33,12 @@ def findStatsDir(subDir: str, name: str):
         "/media/WDC4/",
     ]
     drivesToAppend = []
-    for pd in possibleDrives:
-        if os.path.exists(os.path.join(pd, "BY_PID")):
-            gl = glob(os.path.join(pd, "BY_PID", "*"))
+    for posd in possibleDrives:
+        if os.path.exists(os.path.join(posd, "BY_PID")):
+            gl = glob(os.path.join(posd, "BY_PID", "*"))
             drivesToAppend.extend(gl)
     possibleDrives.extend(drivesToAppend)
-    possibleLocations = [f"{pd}{os.path.sep}{subDir}" for pd in possibleDrives]
+    possibleLocations = [f"{posd}{os.path.sep}{subDir}" for posd in possibleDrives]
     for pl in possibleLocations:
         checkFileName = os.path.join(pl, name, "processed.txt")
         if os.path.exists(checkFileName):
@@ -124,68 +125,91 @@ def main():
         **basicParams
     }
 
-    # get all combinations of parameters in specParams
-    paramValues = list(specParams.values())
-    paramValueCombos = list(product(*paramValues))
-    print(f"Gathering files for {len(paramValueCombos)} param combos")
-
-    correlationNames = [
-        "numStims",
-        "numRipplesPreStats",
-        "numRipplesProbeStats",
-        "stimRate",
-        "rippleRatePreStats",
-        "rippleRateProbeStats",
-    ]
-
-    savedStatsNames = []
-
     testData = False
-
-    epvc = enumerate(paramValueCombos)
-    for ci, combo in tqdm(epvc, total=len(paramValueCombos)):
-        if testData and ci > 10:
-            break
-        params = dict(zip(specParams.keys(), combo))
-        # pprint(params)
-        name = getNameFromParams(specName, params, func, None)
-        noCorrStatsDir = findStatsDir(os.path.join(
-            "figures", "GreatBigDatamining_datamined", "B17", specName), name)
-        # print(f"Found stats dir: {noCorrStatsDir}")
-        statsFiles = glob(os.path.join(noCorrStatsDir, "*.h5"))
-        for sf in statsFiles:
-            savedStatsNames.append((f"{name}_{os.path.basename(sf)}", os.path.abspath(sf)))
-        assert noCorrStatsDir.endswith("/stats")
-
-        for correlationName in correlationNames:
-            name = getNameFromParams(specName, params, func, correlationName)
-            # print(f"Running {name}")
-            likelyStatsDir = noCorrStatsDir[:-6] + f"_X_{correlationName}/stats"
-            if not os.path.exists(likelyStatsDir):
-                statsDir = findStatsDir(os.path.join(
-                    "figures", "GreatBigDatamining_datamined", "B17", specName), name)
-                # print(f"Found stats dir: {statsDir}")
-            else:
-                statsDir = likelyStatsDir
-                # print(f"Found expected stats dir: {likelyStatsDir}")
-            statsFiles = glob(os.path.join(statsDir, "*.h5"))
-            for sf in statsFiles:
-                savedStatsNames.append((f"{name}_{os.path.basename(sf)}", os.path.abspath(sf)))
-
-    # print a random sample of the saved stats names
-    # print("Sample of saved stats names:")
-    # for i in range(10):
-    #     print(random.choice(savedStatsNames))
-
-    shuffler = Shuffler()
     dataDir = findDataDir()
     outputFileName = os.path.join(
-        dataDir, f"{specName}{'' if func is None else '_' + func.__name__}.h5")
-    input("Press enter to start shuffling (output file: {})".format(outputFileName))
+        dataDir, f"{specName}{'' if func is None else '_' + func.__name__}{'_test' if testData else ''}.h5")
 
-    numShuffles = 9 if testData else 100
-    shuffler.runAllShuffles(None, numShuffles=numShuffles,
-                            savedStatsFiles=savedStatsNames, outputFileName=outputFileName)
+    if os.path.exists(outputFileName):
+        print(f"Output file {outputFileName} already exists!")
+    else:
+        # get all combinations of parameters in specParams
+        paramValues = list(specParams.values())
+        paramValueCombos = list(product(*paramValues))
+        print(f"Gathering files for {len(paramValueCombos)} param combos")
+
+        correlationNames = [
+            "numStims",
+            "numRipplesPreStats",
+            "numRipplesProbeStats",
+            "stimRate",
+            "rippleRatePreStats",
+            "rippleRateProbeStats",
+        ]
+
+        savedStatsNames = []
+
+        epvc = enumerate(paramValueCombos)
+        for ci, combo in tqdm(epvc, total=len(paramValueCombos)):
+            if testData and ci > 10:
+                break
+            params = dict(zip(specParams.keys(), combo))
+            # pprint(params)
+            name = getNameFromParams(specName, params, func, None)
+            noCorrStatsDir = findStatsDir(os.path.join(
+                "figures", "GreatBigDatamining_datamined", "B17", specName), name)
+            # print(f"Found stats dir: {noCorrStatsDir}")
+            statsFiles = glob(os.path.join(noCorrStatsDir, "*.h5"))
+            for sf in statsFiles:
+                savedStatsNames.append((f"{name}_{os.path.basename(sf)}", os.path.abspath(sf)))
+            assert noCorrStatsDir.endswith("/stats")
+
+            for correlationName in correlationNames:
+                name = getNameFromParams(specName, params, func, correlationName)
+                # print(f"Running {name}")
+                likelyStatsDir = noCorrStatsDir[:-6] + f"_X_{correlationName}/stats"
+                if not os.path.exists(likelyStatsDir):
+                    statsDir = findStatsDir(os.path.join(
+                        "figures", "GreatBigDatamining_datamined", "B17", specName), name)
+                    # print(f"Found stats dir: {statsDir}")
+                else:
+                    statsDir = likelyStatsDir
+                    # print(f"Found expected stats dir: {likelyStatsDir}")
+                statsFiles = glob(os.path.join(statsDir, "*.h5"))
+                for sf in statsFiles:
+                    savedStatsNames.append((f"{name}_{os.path.basename(sf)}", os.path.abspath(sf)))
+
+        # print a random sample of the saved stats names
+        # print("Sample of saved stats names:")
+        # for i in range(10):
+        #     print(random.choice(savedStatsNames))
+
+        shuffler = Shuffler()
+        input("Press enter to start shuffling (output file: {})".format(outputFileName))
+
+        numShuffles = 9 if testData else 100
+        shuffler.runAllShuffles(None, numShuffles=numShuffles,
+                                savedStatsFiles=savedStatsNames, outputFileName=outputFileName)
+
+    print("Here's the output")
+    # sdf.to_hdf(outputFileName, key="significantShuffles")
+    significantShuffles = pd.read_hdf(outputFileName, key="significantShuffles")
+    print(significantShuffles.head().to_string())
+    print(significantShuffles.shape)
+    # sdf.to_hdf(outputFileName, key="significantCorrelations")
+    significantCorrelations = pd.read_hdf(outputFileName, key="significantCorrelations")
+    print(significantCorrelations.head().to_string())
+    print(significantCorrelations.shape)
+
+    # Next steps:
+    #
+    # Choose a set of parameters to make figures for
+    # chosen set should be "robust" in that modifying any parameter somewhat should not change the conclusions
+    # For probe stats BP, can prefer BPs that include more behavior over less, and ones that don't have inclusion flags
+    # For numerical parameters, plot the effect of changing them on the pvalues over a sampling of the other parameters.
+    # For categorical parameters, plot the effect of changing them on the pvalues over a sampling of the other parameters.
+    # For task measures, I think just have to choose each set of BPs and go through it choosing all the other parameters, and then
+    # select from the BPs at the end
 
 
 if __name__ == "__main__":
